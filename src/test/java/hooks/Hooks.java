6c07fbd76636728f7a9b4bb7391ab8f49076e7d5
Negar France <negar.taajbakhsh@mail.com>
2023-03-12 12:05:24 +0000

    us31APiTest

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -11,19 +11,15 @@ import utilities.Driver;
 
 import static base_url.AppBaseUrl.medunnaSetUp;
 import static base_url.MedunnaBaseUrl.medunnaSetUp2;
-
-
 public class Hooks {
     /*
 Hooks is used to run before and after each SCENARIO or SCENARIO OUTLINE
  */
-
-
     @Before //use cucumber library
     public void setUpScenario() {
+
         System.out.println("Before Method");
     }
-
     @After
     public void tearDownScenario(Scenario scenario) {
 //        System.out.println("After Method");
@@ -33,8 +29,6 @@ Hooks is used to run before and after each SCENARIO or SCENARIO OUTLINE
             Driver.closeDriver();
         }
     }
-
-
     //    This Before hooks ONLY RUNS for @smoke_test TAGGED SCENARIOS
 //    @Before(value = "@smoke_tests")
     @Before("@smoke_tests")
@@ -43,30 +37,23 @@ Hooks is used to run before and after each SCENARIO or SCENARIO OUTLINE
     }
     //This After hooks ONLY RUNS for @smoke_test TAGGED SCENARIOS
 
-
     @After("@smoke_tests")
     public void tearDownSmokeScenarios() {
-
         System.out.println("RUN FOR ONLY SMOKE TEST SCENARIOS");
     }
-
     @Before("@US08")
     public void beforeApi() {  // This method will run before Api Tests
         medunnaSetUp();
 
     }
-
     @Before("@Api_Test")
     public void beforeApi3() {
         medunnaSetUp2();     // This method actually works If you use the new one you will get 500 error.
-
     }
-    @Before("@US31_Api")
-    public void beforeApi2() {
-        medunnaSetUp2();     // This method actually works If you use the new one you will get 500 error.
-
-    }
-
-
+//    @Before("@US31_Api")
+//    public void beforeApi2() {
+//        medunnaSetUp2();     // This method actually works If you use the new one you will get 500 error.
+//
+//    }
 
 }
