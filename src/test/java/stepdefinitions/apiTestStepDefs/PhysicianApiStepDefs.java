3867014fb93065b429aec715794424132d0e1258
Yasin Yılmaz <yasinyilmazzz@gmail.com>
2023-03-14 17:50:10 +0300

    last

--- src/test/java/stepdefinitions/apiTestStepDefs/PhysicianApiStepDefs.java
+++ src/test/java/stepdefinitions/apiTestStepDefs/PhysicianApiStepDefs.java
@@ -10,31 +10,55 @@ import pages.PhysicianPage;
 import stepdefinitions.UITestStepDefs.RoomPageStepDefs;
 import utilities.MedunnaAuthentication;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
 public class PhysicianApiStepDefs {
 
     PhysicianPage physicianPage = new PhysicianPage();
+    static Response response;
+    static JsonPath json;
+    static List<Integer> doctorList;
+    public static String expectedData = "";
+    String url ="https://medunna.com/api/physicians?size=85";
+    static int actualDataApi;
+
+    public static String getExpectedData() {
+        return expectedData;
+    }
 
     @When("Admin get count of physicians")
     public void admin_get_count_of_physicians() {
-        String countOfItems= physicianPage.physicianCount.getText();
-        String[] listOfString = countOfItems.split("");
-        System.out.println(listOfString);
+        String countOfItems = physicianPage.physicianCount.getText();
+        System.out.println(countOfItems);
+        String[] listOfString = countOfItems.split(" ");
+        expectedData = listOfString[5];
+        System.out.println("expectedData = " + expectedData);
 
     }
+
     @When("Admin send get request to API")
     public void admin_send_get_request_to_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        response = given().headers("Authorization","Bearer "+ MedunnaAuthentication.generateToken()).contentType(ContentType.JSON).get(url);
+        json = response.jsonPath();
+        doctorList = json.getList("id");
+
     }
+
     @When("Admin get physicians count from API")
     public void admin_get_physicians_count_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actualDataApi = doctorList.size();
+        System.out.println("actualDataApi = " + actualDataApi);
     }
+
     @Then("Admin verify physicians count from UI equals with count from API")
     public void admin_verify_physicians_count_from_ui_equals_with_count_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String actualData = String.valueOf(actualDataApi);
+        Assert.assertEquals(expectedData,actualData);
     }
 
 }
